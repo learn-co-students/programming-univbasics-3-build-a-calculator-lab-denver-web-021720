52082771d623514c1ab0b8efbacdf68884919a06
Brandon Green <brandon@brandonleeproductions.com>
2020-01-03 01:16:57 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  value = num1 - num2
+  value
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  sum
 end
 
 def square_root(num)
-
+  sum = Math.sqrt(num)
+  sum
 end
